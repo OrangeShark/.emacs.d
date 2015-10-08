83e5fed3ec34c407129d3b04f7a5d3cd3721985d
Erik Edrosa <erik.edrosa@gmail.com>
2015-10-08 01:28:42 -0400

    Add check for quicklisp slime-helper

--- customizations/setup-lisp.el
+++ customizations/setup-lisp.el
@@ -1,10 +1,11 @@
 
 
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-
-;; Slime set up
-(setq inferior-lisp-program "sbcl")
-
-(slime-setup '(slime-company))
+(defvar *slime-helper-file* "~/quicklisp/slime-helper.el")
+(when (file-exists-p *slime-helper-file*)
+  (progn
+    (load (expand-file-name *slime-helper-file*))
+    ;; Slime set up
+    (setq inferior-lisp-program "sbcl")
+    (slime-setup '(slime-company))))
 
 (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode)
